43b5e03f26824e730e215632fe873cffdfc77207
David Manjavacas <d.manjavacas@docu.com>
2020-08-16 19:10:30 +0200

    First commit

---
+++ js/index.js
@@ -0,0 +1,84 @@
+setInterval(() => {
+    //document.querySelector('body').style.backgroundColor = getRandomColor();
+}, 2000);
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  
+(function ($) {
+  "use strict";
+
+
+  /*==================================================================
+  [ Focus Contact2 ]*/
+  $('.input2').each(function(){
+      $(this).on('blur', function(){
+          if($(this).val().trim() != "") {
+              $(this).addClass('has-val');
+          }
+          else {
+              $(this).removeClass('has-val');
+          }
+      })    
+  })
+          
+
+  
+  /*==================================================================
+  [ Validate ]*/
+  var name = $('.validate-input input[name="name"]');
+  var email = $('.validate-input input[name="email"]');
+  var message = $('.validate-input textarea[name="message"]');
+
+
+  $('.validate-form').on('submit',function(){
+      var check = true;
+
+      if($(name).val().trim() == ''){
+          showValidate(name);
+          check=false;
+      }
+
+
+      if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+          showValidate(email);
+          check=false;
+      }
+
+      if($(message).val().trim() == ''){
+          showValidate(message);
+          check=false;
+      }
+
+      return check;
+  });
+
+
+  $('.validate-form .input2').each(function(){
+      $(this).focus(function(){
+         hideValidate(this);
+     });
+  });
+
+  function showValidate(input) {
+      var thisAlert = $(input).parent();
+
+      $(thisAlert).addClass('alert-validate');
+  }
+
+  function hideValidate(input) {
+      var thisAlert = $(input).parent();
+
+      $(thisAlert).removeClass('alert-validate');
+  }
+  
+  
+
+})(jQuery);
